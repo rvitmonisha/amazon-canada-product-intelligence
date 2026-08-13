295eec87daf75bd81e156f941bff9cea79dd89b9
rvitmonisha <mnmonisha39@gmail.com>
2026-08-13 22:49:33 +0530

    feat: harden Amazon Canada product scraper

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,5 +1,113 @@
-from backend.app.scraper.amazon import scrape_product
+import pytest
+
+from backend.app.amazon_scraper import (
+    clean_amazon_url,
+    extract_price,
+    extract_rating,
+    extract_reviews,
+    validate_amazon_url,
+)
 
 
-def test_scraper_returns_product_structure():
-    assert callable(scrape_product)
+def test_clean_amazon_url():
+    url = "[Amazon Product](https://www.amazon.ca/dp/B0GSHQ525L)"
+
+    result = clean_amazon_url(url)
+
+    assert result == "https://www.amazon.ca/dp/B0GSHQ525L"
+
+
+def test_clean_amazon_url_without_markdown():
+    url = "https://www.amazon.ca/dp/B0GSHQ525L"
+
+    result = clean_amazon_url(url)
+
+    assert result == url
+
+
+def test_validate_amazon_url():
+    validate_amazon_url(
+        "https://www.amazon.ca/dp/B0GSHQ525L"
+    )
+
+
+def test_invalid_amazon_url():
+    with pytest.raises(ValueError):
+        validate_amazon_url(
+            "https://www.amazon.com/dp/B0GSHQ525L"
+        )
+
+
+def test_extract_price():
+    from bs4 import BeautifulSoup
+
+    html = """
+    <span class="a-price">
+        <span class="a-offscreen">$1,299.99</span>
+    </span>
+    """
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    assert extract_price(soup) == 1299.99
+
+
+def test_extract_rating():
+    from bs4 import BeautifulSoup
+
+    html = """
+    <span class="a-icon-alt">
+        4.6 out of 5 stars
+    </span>
+    """
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    assert extract_rating(soup) == 4.6
+
+
+def test_extract_reviews():
+    from bs4 import BeautifulSoup
+
+    html = """
+    <span id="acrCustomerReviewText">
+        1,234 ratings
+    </span>
+    """
+
+    soup = BeautifulSoup(html, "html.parser")
+
+    assert extract_reviews(soup) == 1234
+
+
+def test_missing_price():
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(
+        "<html></html>",
+        "html.parser"
+    )
+
+    assert extract_price(soup) is None
+
+
+def test_missing_rating():
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(
+        "<html></html>",
+        "html.parser"
+    )
+
+    assert extract_rating(soup) is None
+
+
+def test_missing_reviews():
+    from bs4 import BeautifulSoup
+
+    soup = BeautifulSoup(
+        "<html></html>",
+        "html.parser"
+    )
+
+    assert extract_reviews(soup) == 0
